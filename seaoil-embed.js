d05ebf9810da8bce1459bad0b6d47ad61e30a985
j-cochran <jon.cochran@staffbase.com>
2024-12-16 14:01:04 +1100

    Initial commit
    Added Chatbase embed js file for SeaOil chatbot

---
+++ seaoil-embed.js
@@ -0,0 +1,12 @@
+(function(d, w) {
+    w.embeddedChatbotConfig = {
+        chatbotId: "HA48MELIhdEbPnQVjF3QT",
+        domain: "www.chatbase.co"
+    };
+    var s = d.createElement("script");
+    s.src = "https://www.chatbase.co/embed.min.js";
+    s.chatbotId="HA48MELIhdEbPnQVjF3QT";
+    s.domain="www.chatbase.co";
+    s.async = true;
+    d.head.appendChild(s);
+})(document, window);
